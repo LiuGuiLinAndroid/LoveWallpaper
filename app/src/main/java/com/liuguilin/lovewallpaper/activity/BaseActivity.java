f475d20f6a521490c030a8cbb2c5b60159cb64b3
LiuGuiLinAndroid <748778890@qq.com>
2017-01-09 20:12:16 +0800

    新增 设置

--- app/src/main/java/com/liuguilin/lovewallpaper/activity/BaseActivity.java
+++ app/src/main/java/com/liuguilin/lovewallpaper/activity/BaseActivity.java
@@ -10,11 +10,24 @@ package com.liuguilin.lovewallpaper.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
---
+++ app/src/main/java/com/liuguilin/lovewallpaper/activity/SettingActivity.java
@@ -0,0 +1,22 @@
+package com.liuguilin.lovewallpaper.activity;
+/*
+ *  项目名：  LoveWallpaper 
+ *  包名：    com.liuguilin.lovewallpaper.activity
+ *  文件名:   SettingActivity
+ *  创建者:   LGL
+ *  创建时间:  2017/1/9 19:47
+ *  描述：    设置
+ */
+
+import android.os.Bundle;
+
+import com.liuguilin.lovewallpaper.R;
+
+public class SettingActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+}
