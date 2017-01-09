3f1de3a36b8f10ddbf255e7c7f76aa13ac7695f3
LiuGuiLinAndroid <748778890@qq.com>
2017-01-09 18:09:50 +0800

    新增Base类

---
+++ app/src/main/java/com/liuguilin/lovewallpaper/activity/BaseActivity.java
@@ -0,0 +1,20 @@
+package com.liuguilin.lovewallpaper.activity;
+/*
+ *  项目名：  LoveWallpaper 
+ *  包名：    com.liuguilin.lovewallpaper.activity
+ *  文件名:   BaseActivity
+ *  创建者:   LGL
+ *  创建时间:  2017/1/9 18:09
+ *  描述：    基类
+ */
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
