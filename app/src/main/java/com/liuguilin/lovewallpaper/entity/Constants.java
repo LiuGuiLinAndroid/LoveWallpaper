a2941881fa83e07e0cdac4786165338e9812e995
LiuGuiLinAndroid <748778890@qq.com>
2017-01-09 17:25:55 +0800

    first commit

---
+++ app/src/main/java/com/liuguilin/lovewallpaper/entity/Constants.java
@@ -0,0 +1,13 @@
+package com.liuguilin.lovewallpaper.entity;
+/*
+ *  项目名：  LoveWallpaper 
+ *  包名：    com.liuguilin.lovewallpaper.entity
+ *  文件名:   Constants
+ *  创建者:   LGL
+ *  创建时间:  2017/1/9 17:16
+ *  描述：    常量类
+ */
+
+public class Constants {
+
+}
